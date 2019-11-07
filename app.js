193d0180020e475bb58521b7f7505fedf88c05d5
Hasan Tekin <hasantekin_@hotmail.com.tr>
2019-11-07 18:59:40 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+//Başlangıçta benim profilimde yazan her şey anlamsız karakterlerden oluşsun. Aşağıdaki gibi ama daha güzel görünen.
+
+//https://coolsymbol.com/
+//bu siteden alınabilir.
+
+//#$^^&#%&^%**^#%*#$%&%$&%$*%$&%$^#^%*$#%#@%$%&%$$#&**()&)($^%&%$^@$%!^$%^$%^^%@^%@%&#%&&%#%^*@%$^$#%@*
+
+//Web URL'nin sonuna ?number= query stringi eklendiği zaman sayfa Webrtc ile birbirine bağlanmaya çalışıyor library üzerinden.
+//number=462 ise bağlanmaya çalıştıkları odanın adı "flying462sheep" olabilir.
+//İki tab birbirine bağlanınca arka plan renklerinin yeri tam zıttı olsun ikisinde de.
+//Birisinde mouse hareket ettirince diğerinde bir top hareket etsin
