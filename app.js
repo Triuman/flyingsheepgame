75248d48fa12a12dade6c7f25d2942b48803a5dd
Hasan Tekin <hasantekin_@hotmail.com.tr>
2019-11-10 02:01:23 +0100

    IO connects and hand moves

--- app.js
+++ app.js
@@ -11,9 +11,54 @@
 //İki tab birbirine bağlanınca arka plan renklerinin yeri tam zıttı olsun ikisinde de.
 //Birisinde mouse hareket ettirince diğerinde bir top hareket etsin
 
+const io = require('socket.io')();
 const express = require('express');
 var app = express();
 var http = require('http').createServer(app);
+
+io.attach(http, {
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false
+});
+
+var rooms = {};
+
+io.on('connection', (socket) => {
+  console.log("socket connected!");
+  socket.on('register', (key) => {
+    if (key) {
+      socket.gameKey = key;
+      rooms[key] = rooms[key] || [];
+      if (rooms[key].length >= 2) {
+        socket.disconnect();
+        return;
+      }
+      rooms[key].push(socket);
+      if (rooms[key].length === 2) {
+        var otherSocket = rooms[key][0];
+        otherSocket.otherSocket = socket;
+        socket.otherSocket = otherSocket;
+        console.log("sockets are connected to each other with key " + key);
+      }
+    }
+  });
+
+  socket.on('mouse', (pos) => {
+    if (socket.otherSocket) {
+      socket.otherSocket.emit("mouse", pos);
+    }
+  });
+  socket.on('disconnect', (reason) => {
+    rooms[socket.gameKey] = null;
+    if (socket.otherSocket) {
+      rooms[socket.gameKey] = [socket.otherSocket];
+      socket.otherSocket.otherSocket = null;
+      socket.otherSocket = null;
+    }
+  });
+});
+
 
 app.use(express.static('public'));
 
@@ -21,6 +66,6 @@ app.use(express.static('public'));
 //   res.sendFile(__dirname + '/index.html');
 // });
 const port = process.env.PORT || 3000;
-http.listen(port, function(){
+http.listen(port, function () {
   console.log('listening on *:' + port);
 });
