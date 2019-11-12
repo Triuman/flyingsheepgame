08aa429bbe93849cc6dd9a4a6937f0e81e529ae1
hasantekin <hasan.tekin@serviceware.se>
2019-11-12 17:46:03 +0100

    port 80

--- app.js
+++ app.js
@@ -217,7 +217,7 @@ app.use(express.static('public'));
 // app.get('/', function(req, res){
 //   res.sendFile(__dirname + '/index.html');
 // });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 http.listen(port, function () {
   log.info('listening on *:' + port);
 });
